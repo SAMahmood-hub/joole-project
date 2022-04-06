0e195dc3e4bb6e7f7ddb96fe77ced91e49643a97
SAMahmood-hub <83553171+SAMahmood-hub@users.noreply.github.com>
2022-04-06 12:52:40 -0400

    Deleted extra files

--- src/main/java/com/itlizeSession/product/Repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.itlizeSession.product.Repository;
-
-import com.itlizeSession.product.Entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project,Integer>{
-
-}
